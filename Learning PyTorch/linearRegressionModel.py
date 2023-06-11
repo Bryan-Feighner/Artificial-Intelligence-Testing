b299ffb5a64e216677be04193323e41925a1d232
Bryan-Feighner <bfeighne@stevens.edu>
2023-06-11 12:41:23 -0400

    updated linearRegressionModel

--- Learning PyTorch/linearRegressionModel.py
+++ Learning PyTorch/linearRegressionModel.py
@@ -1,1 +1,10 @@
 import torch
+import torch.nn as nn
+from torch.autograd import Variable
+xTrain = x.reshape(-1,1).asType('float32')
+xTrain = y.reshape(-1,1).asTypw('float32')
+
+class linearRegressionModel(nn.Module):
+    def __init__(self, inputDimension, outputDimension):
+        super(linearRegressionModel, self).__init__()
+        self.linear = nn.Linear(inputDimension, outputDimension)
